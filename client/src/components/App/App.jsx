8ff15b2945baccd7e8e1b7211c5c00141d211431
dianalynshull <dianalynshull@gmail.com>
2021-01-05 22:23:13 -0700

    added pages to the App component and installed react-router-dom to use Router

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,15 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
+import Homepage from '../../pages/Homepage';
+import ProjectPortfolio from '../../pages/ProjectPortfolio';
+import Contact from '../../pages/Contact';
 
 import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
-      This is the App component
+      <div>
+        <Switch>
+          <Route exact path='/' component={ Homepage } />
+          <Route exact path='/portfolio' component={ ProjectPortfolio } />
+          <Route exact path='/contact' component={ Contact } />
+        </Switch>
+      </div>
       <Footer />
-    </div>
+    </Router>
+
   );
 }
 
