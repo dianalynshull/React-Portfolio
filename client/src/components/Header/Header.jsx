a6ed6d7e032c379b15ff746893a1f3aad3ceac06
dianalynshull <dianalynshull@gmail.com>
2021-01-05 22:08:10 -0700

    created the header navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap'
+
 const Header = () => {
   return (
-    <div>
-      This is the header component
-    </div>
+    <>
+      <Navbar bg="light" expand="md">
+        <Navbar.Brand href="/homepage">Diana Shull</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/homepage">Homepage</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/link">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 
