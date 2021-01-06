f2f50eeb63b729533ca856c8a5ecd1cce5ea1a2d
dianalynshull <dianalynshull@gmail.com>
2021-01-05 23:26:34 -0700

    added header styles and added a footer style to prevent the footer from missing opacity

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <>
-      <Navbar bg="dark" expand="md" className="fixed-bottom">
+      <Navbar bg="dark" expand="md" className="footer fixed-bottom">
           <Nav className="m-auto">
             <Nav.Item><i className="far fa-copyright"></i><span className="copyright"> Copyright</span></Nav.Item>
           </Nav>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap';
+import './Header.css';
 
 const Header = () => {
   return (
     <>
-      <Navbar bg="light" expand="md">
-        <Navbar.Brand href="/">Diana Shull</Navbar.Brand>
+      <Navbar bg="dark" expand="md" className="navbar-dark">
+        <Navbar.Brand className="navbar-brand" href="/">Diana Shull</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
