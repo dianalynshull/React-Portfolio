c7bb1acf8980e637bf637c45ec406b8441b1f0dd
dianalynshull <dianalynshull@gmail.com>
2021-01-05 22:25:31 -0700

    corrected the route to the homepage

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <>
       <Navbar bg="light" expand="md">
-        <Navbar.Brand href="/homepage">Diana Shull</Navbar.Brand>
+        <Navbar.Brand href="/">Diana Shull</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/homepage">Homepage</Nav.Link>
+            <Nav.Link href="/">Homepage</Nav.Link>
             <Nav.Link href="/portfolio">Portfolio</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
             <Nav.Link href="/link">Resume</Nav.Link>
