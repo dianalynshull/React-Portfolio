1fbcd96e319145f4bb1b6dfc5276d33a123744e3
dianalynshull <dianalynshull@gmail.com>
2021-01-05 22:18:04 -0700

    added the copyright symbol to footer and also added the font-awesome script

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
       <Navbar bg="light" expand="md" className="fixed-bottom">
           <Nav className="m-auto">
-            <Nav.Link href="/homepage">Copyright</Nav.Link>
+            <Nav.Link href="/homepage"><i class="far fa-copyright"></i><span class="copyright"> Copyright</span></Nav.Link>
           </Nav>
       </Navbar>
     </>
