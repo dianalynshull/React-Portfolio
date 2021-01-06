60fd5f783102878d5533cd84311b1f64e1c90744
dianalynshull <dianalynshull@gmail.com>
2021-01-06 03:26:17 -0700

    added the option to serve index.html if the application is running in production. also added link to my resume in the top nav

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const routes = require('routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,7 +6,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(routes);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log('Server is running on PORT: ' + PORT);
