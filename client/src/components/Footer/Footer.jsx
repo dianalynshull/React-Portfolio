d02e13aab0022f6f500299e79e184a63d32a8da4
dianalynshull <dianalynshull@gmail.com>
2021-01-05 23:17:24 -0700

    added footer styles

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
+import './Footer.css';
 
 const Footer = () => {
   return (
     <>
-      <Navbar bg="light" expand="md" className="fixed-bottom">
+      <Navbar bg="dark" expand="md" className="fixed-bottom">
           <Nav className="m-auto">
             <Nav.Item><i className="far fa-copyright"></i><span className="copyright"> Copyright</span></Nav.Item>
           </Nav>
